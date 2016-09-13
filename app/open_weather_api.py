e57a6f81ea21fb71f08870f919b526502885f5d2
Matt Smith <msmith@apaxsoftware.com>
2016-09-13 09:01:58 -0400

    Return a consistent DailyWeather object from API calls and refactor templates to use the new object.

--- app/open_weather_api.py
+++ app/open_weather_api.py
@@ -29,7 +29,19 @@ class OpenWeatherAPI():
         payload = self.get_payload(q=q)
         url = "{}{}".format(self.base_url, "/weather")
         r = requests.get(url, params=payload)
-        return weather_items_or_error(r.json())
+        result =  result_or_error(r.json())
+
+        return DailyWeather(
+            icon = result['weather'][0]['icon'],
+            description = result['weather'][0]['description'],
+            temp = result['main']['temp'],
+            dt = result['dt'],
+            wind = result['wind']['speed'],
+            pressure = result['main']['pressure'],
+            humidity = result['main']['humidity'],
+            sunrise = result['sys']['sunrise'],
+            sunset = result['sys']['sunset']
+        )
 
     def get_daily_weather(self, city, country_code="", num_days=16):
         q = ""
@@ -42,10 +54,49 @@ class OpenWeatherAPI():
         url = "{}{}".format(self.base_url, "/forecast/daily")
         r = requests.get(url, params=payload)
 
-        return weather_items_or_error(r.json())
+        result = result_or_error(r.json())
 
-def weather_items_or_error(weather_items):
-    if str(weather_items.get("cod")) != "200":
+        weather_list = []
+        for item in result["list"]:
+            daily_weather = DailyWeather(
+                icon =item["weather"][0]["icon"],
+                description = item["weather"][0]["description"],
+                temp = item["temp"]["day"],
+                temp_night = item["temp"]["night"],
+                dt = item["dt"],
+                pressure = item["pressure"],
+                humidity = item["humidity"],
+            )
+            weather_list.append(daily_weather)
+
+        return weather_list
+
+def result_or_error(result):
+    if str(result.get("cod")) != "200":
         raise ValueError("Not a valid city")
 
-    return weather_items
+    return result
+
+class DailyWeather():
+    def __init__(self,
+        description="",
+        icon=None,
+        dt=None,
+        temp=None,
+        temp_night=None,
+        wind=None,
+        pressure=None,
+        humidity=None,
+        sunrise=None,
+        sunset=None):
+
+        self.description = description
+        self.icon = icon
+        self.dt = dt
+        self.temp = temp
+        self.temp_night = temp_night
+        self.wind = wind
+        self.pressure = pressure
+        self.humidity = humidity
+        self.sunrise = sunrise
+        self.sunset = sunset
--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ def index():
 @app.route("/current", methods=["GET", "POST"])
 def current_weather():
     weather_form = WeatherForm(request.form)
-    weather = None
+    weather_item = None
     city = ""
     country_code = ""
 
@@ -23,20 +23,20 @@ def current_weather():
 
         try:
             api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"])
-            weather = api.get_current_weather(city, country_code)
+            weather_item = api.get_current_weather(city, country_code)
         except ValueError:
             flash("City Not Found")
 
     return render_template("current.html",
         weather_form=weather_form,
-        weather=weather,
+        weather_item=weather_item,
         city=city,
         country_code=country_code)
 
 @app.route("/forecast", methods=["GET", "POST"])
 def forecast_weather():
     weather_form = WeatherForm(request.form)
-    weather = {}
+    weather_list = None
     city = ""
     country_code = ""
 
@@ -47,13 +47,13 @@ def forecast_weather():
 
         try:
             api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"])
-            weather = api.get_daily_weather(city, country_code)
+            weather_list = api.get_daily_weather(city, country_code)
         except ValueError:
-            weather = {}
+            weather_list = None
             flash("City Not Found")
 
     return render_template("forecast.html",
         weather_form=weather_form,
-        weather_list=weather.get("list"),
+        weather_list=weather_list,
         city=city,
         country_code=country_code)
