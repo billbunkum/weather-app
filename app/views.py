91768cf255a1d5eae7f78bfd7e04c617cd85c29c
Matt Smith <msmith@apaxsoftware.com>
2016-09-12 21:35:30 -0400

    Display current weather.

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from flask import render_template, request
 
 from app import app
 from app.forms import WeatherForm
+from app.open_weather_api import OpenWeatherAPI
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -10,12 +12,22 @@ def index():
 @app.route("/current", methods=["GET", "POST"])
 def current_weather():
     weather_form = WeatherForm(request.form)
+    weather = None
+    city = ""
+    country_code = ""
 
     if request.method == "POST" and weather_form.validate():
         # do stuff if form is valid
-        pass
+        city = weather_form.city.data
+        country_code = weather_form.country_code.data
 
-    return render_template("current.html", weather_form=weather_form)
+        api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"])
+        weather = api.get_current_weather(city, country_code)
+    return render_template("current.html",
+        weather_form=weather_form,
+        weather=weather,
+        city=city,
+        country_code=country_code)
 
 @app.route("/forecast", methods=["GET", "POST"])
 def forecast_weather():
